0585adf6c073d73faf1144f13440bef0d3fb3afd
unknown <yvonne@ALTAIR.fritz.box>
2020-05-11 16:04:12 +0200

    Create new app service

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,17 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Board } from '../models/Board';
+import { GameBoardService } from '../service/game-board.service';
 import { Field } from '../models/Field';
-import { Carrier } from '../models/ships/Carrier';
-import { Coordinate } from '../models/Coordinate';
-import { Direction, Ship } from '../models/ships/Ship';
-import { Battleship } from '../models/ships/Battleship';
-import { Destroyer } from '../models/ships/Destroyer';
-import { Submarine } from '../models/ships/Submarine';
-import { PatrolBoat } from '../models/ships/PatrolBoat';
-import { StatusImage } from '../models/StatusImage';
-
-const HEADER_HORIZONTAL = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'];
-const HEADER_VERTICAL = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];
 
 @Component({
   selector: 'app-game-board',
@@ -20,68 +9,29 @@ const HEADER_VERTICAL = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'
 })
 export class GameBoardComponent implements OnInit {
 
-  gameBoard: Board = new Board(10, 10);
-  failedShots = 0;
-
-  // TODO: Randomize ship positions
-  carrier = new Carrier(new Coordinate(2, 5), Direction.Right);
-  battleship = new Battleship(new Coordinate(6, 3), Direction.Up);
-  destroyer = new Destroyer(new Coordinate(5, 8), Direction.Left);
-  submarine = new Submarine(new Coordinate(1, 1), Direction.Down);
-  patrolBoat = new PatrolBoat(new Coordinate(0, 8), Direction.Down);
-
-  constructor() { }
+  constructor(private gameBoardService: GameBoardService) { }
 
   ngOnInit() {
-    this.setShips();
+    this.gameBoardService.initializeBoard();
   }
 
-  shoot(field: Field) {
-    if (field.isShot) {
-      // field already clicked -> do nothing
-      return;
-    }
-    field.isShot = true;
-    if (field.ship != null) {
-      field.statusImage = StatusImage.SHIP;
-      field.ship.hit();
-      if (field.ship.isSunken()) {
-        this.killShip(field.ship);
-      }
-    } else {
-      this.failedShots++;
-      field.statusImage = StatusImage.SEA_SHOT;
-    }
-  }
-
-  killShip(ship: Ship) {
-    this.getFieldsAsFlattenArray().forEach((field: Field) => {
-      if (field.ship === ship) {
-        field.statusImage = StatusImage.SHIP_KILLED;
-      }
-    });
-  }
-
-  getFieldImage(field: Field): string {
-    if ( field.isShot ) {
-      return field.statusImage;
-    }
-    return StatusImage.SEA;
-  }
-
-  getFieldsAsFlattenArray() {
-    return [].concat.apply([], this.gameBoard.fields);
+  getFailedShots(): number {
+    return this.gameBoardService.getFailedShots();
   }
 
   getHorizontalHeader() {
-    return HEADER_HORIZONTAL.slice(0, this.gameBoard.getLength());
+    return this.gameBoardService.getHorizontalHeader();
   }
 
-  setShips() {
-    this.gameBoard.addShip(this.carrier);
-    this.gameBoard.addShip(this.battleship);
-    this.gameBoard.addShip(this.destroyer);
-    this.gameBoard.addShip(this.submarine);
-    this.gameBoard.addShip(this.patrolBoat);
+  getGameBoardWidth(): number {
+    return this.gameBoardService.getWidth();
+  }
+
+  getFields() {
+    return this.gameBoardService.getFields();
+  }
+
+  shoot(field: Field) {
+    this.gameBoardService.shoot(field);
   }
 }

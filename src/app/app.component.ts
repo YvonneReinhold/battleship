e2d1f54bca85a5273f3834a7c46747ca5b818dbc
unknown <yvonne@ALTAIR.fritz.box>
2020-06-02 10:33:44 +0200

    Add start and reset button to game panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  startGame: boolean;
+  resetGame: boolean;
+
+  @Output() resetGameBoardEvent = new EventEmitter<any>();
+
+  ngOnInit(): void {
+    this.startGame = false;
+    this.resetGame = false;
+  }
+
+  startButtonClicked() {
+    this.startGame = true;
+    console.log('startButtonClicked()');
+  }
+
+  isStartButtonClicked(): boolean {
+    return this.startGame;
+  }
+
+  resetButtonClicked() {
+    this.resetGame = true;
+    console.log('resetButtonClicked()');
+  }
+
+  isResetButtonClicked(): boolean {
+    return this.startGame;
+  }
 }

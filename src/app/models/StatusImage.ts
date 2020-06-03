4ac0320ef3e771277bf5973205253dfe18cf8218
unknown <yvonne@ALTAIR.fritz.box>
2020-06-03 15:43:01 +0200

    Remove unused image entry

--- src/app/models/StatusImage.ts
+++ src/app/models/StatusImage.ts
@@ -1,5 +1,4 @@
 export enum StatusImage {
-  SEA = '../../assets/sea-water.jpg',
   SEA_SHOT = '../../assets/failed-shot.png',
   SHIP_KILLED = '../../assets/ship-killed.png',
   SHIP = '../../assets/ship.png'
